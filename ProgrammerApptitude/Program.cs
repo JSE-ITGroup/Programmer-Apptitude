a65cbdfcba5fc05e81ae5e0c8b6bea711e6b43d8
Michael Edwards <michael.edwards@jamstockex.com>
2018-05-28 13:06:53 -0500

    Commit of initial files

---
+++ ProgrammerApptitude/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerApptitude
+{    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(6.5));
+            Console.WriteLine(Math.Round(11.5));
+            Console.ReadLine();
+        }
+    }
+}
